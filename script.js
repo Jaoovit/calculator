d7646252cc082cf3966e9bf0a3cb8d7c5ebf2a5c
João Oliveira <jaoovit@gmail.com>
2024-01-15 09:07:58 +0000

    create a eventlistener to populate fistnumber and secondnumber variables

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const six = document.querySelector('#six')
 const seven = document.querySelector('#seven')
 const eight = document.querySelector('#eight')
 const nine = document.querySelector('#nine')
+const dot = document.querySelector('#dot')
+const zero = document.querySelector('#zero')
 
 const scream = document.querySelector('#scream')
 
@@ -36,32 +38,119 @@ function divide(a,b) {
     return a / b
 }
 
-///
+let firstNumber = ''
+let secondNumber = ''
 
-let firstNumber = '2'
-let secondNumber = '1'
+one.addEventListener('click', function onClick() {
+    scream.innerText += '1'
+    if (operator == '') {
+        return firstNumber += '1'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '1'
+    
+})
+
+two.addEventListener('click', function onClick() {
+    scream.innerText += '2'
+    if (operator == '') {
+        return firstNumber += '2'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '2'
+    
+})
+
+three.addEventListener('click', function onClick() {
+    scream.innerText += '3'
+    if (operator == '') {
+        return firstNumber += '3'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '3'
+    
+})
+
+four.addEventListener('click', function onClick() {
+    scream.innerText += '4'
+    if (operator == '') {
+        return firstNumber += '4'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '4'
+    
+})
+
+five.addEventListener('click', function onClick() {
+    scream.innerText += '5'
+    if (operator == '') {
+        return firstNumber += '5'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '5'
+    
+})
+
+six.addEventListener('click', function onClick() {
+    scream.innerText += '6'
+    if (operator == '') {
+        return firstNumber += '6'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '6'
+    
+})
+
+seven.addEventListener('click', function onClick() {
+    scream.innerText += '7'
+    if (operator == '') {
+        return firstNumber += '7'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '7'
+    
+})
+
+eight.addEventListener('click', function onClick() {
+    scream.innerText += '8'
+    if (operator == '') {
+        return firstNumber += '8'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '8'
+    
+})
+
+nine.addEventListener('click', function onClick() {
+    scream.innerText += '9'
+    if (operator == '') {
+        return firstNumber += '9'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '9'
+    
+})
+
+zero.addEventListener('click', function onClick() {
+    scream.innerText += '0'
+    if (operator == '') {
+        return firstNumber += '0'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '0'
+    
+})
+
+dot.addEventListener('click', function onClick() {
+    scream.innerText += '.'
+    if (operator == '') {
+        return firstNumber += '.'
+    } else if (operator == '+' || '-' || '*' || '/') return secondNumber += '.'
+    
+})
+
 let operator = ''
 
 sum.addEventListener('click', function onClick() {
-    scream.innerText = '+'
+    scream.innerText += '+'
     operator = '+'
 })
 subt.addEventListener('click', function onClick() {
-    scream.innerText = '-'
+    scream.innerText += '-'
     operator = '-'
 })
 mult.addEventListener('click', function onClick() {
-    scream.innerText = '*'
+    scream.innerText += '*'
     operator = '*'
 })
 div.addEventListener('click', function onClick() {
-    scream.innerText = '/'
+    scream.innerText += '/'
     operator = '/'
 })
 
-///
-
 function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
     switch (operator) {
         case '+':
             return add(a,b)
@@ -77,9 +166,7 @@ function operate(operator, a, b) {
     }
 }
 
-console.log(operate(operator.multiply, Number(firstNumber), Number(secondNumber)))
-
-
-    equal.addEventListener('click', function onClick() {
-        scream.innerText = operate(operator, Number(firstNumber), Number(secondNumber));
-    })
+equal.addEventListener('click', function onClick() {
+    scream.innerText = operate(operator, Number(firstNumber), Number(secondNumber));
+    console.log(firstNumber,secondNumber,operator)
+})
